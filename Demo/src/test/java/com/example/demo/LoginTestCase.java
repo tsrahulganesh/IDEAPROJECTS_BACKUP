b39b0246f4ad472747f16d016959bc817cd8ec81
rrahul <tsrahulganesh@gmail.com>
2025-12-30 23:25:16 +0530

    commit

--- Demo/src/test/java/com/example/demo/LoginTestCase.java
+++ Demo/src/test/java/com/example/demo/LoginTestCase.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.openqa.selenium.WebDriver;
 
-public class Login_TestCases {
+public class LoginTestCase {
 
     public void testCase1_ValidLogin() {
         WebDriver driver = DriverManager.getDriver();
